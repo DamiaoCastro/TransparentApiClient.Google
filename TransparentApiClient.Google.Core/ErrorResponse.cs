03033d7319387b289c59d1ada33956adb87b5e5b
dcastro@travix.com <dcastro@travix.com>
2018-02-19 19:12:30 +0100

    add error response

--- TransparentApiClient.Google.Core/ErrorResponse.cs
+++ TransparentApiClient.Google.Core/ErrorResponse.cs
@@ -2,12 +2,12 @@
 {
     public class ErrorResponse
     {
-        public Error1[] errors { get; set; }
+        public Error[] errors { get; set; }
         public int code { get; set; }
         public string message { get; set; }
     }
 
-    public class Error1
+    public class Error
     {
         public string domain { get; set; }
         public string reason { get; set; }
