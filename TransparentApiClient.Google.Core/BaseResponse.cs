5cf04f6c1b295e80771996ed31a305b213e659e6
damiao <damiao.c@hotmail.com>
2018-09-20 18:20:30 +0200

    create constructor for BaseResponse in order to be testable

--- TransparentApiClient.Google.Core/BaseResponse.cs
+++ TransparentApiClient.Google.Core/BaseResponse.cs
@@ -4,6 +4,16 @@ namespace TransparentApiClient.Google.Core {
 
     public class BaseResponse<S> {
 
+        public BaseResponse() { }
+
+        public BaseResponse(bool success, S response, HttpStatusCode responseCode, ErrorResponse error, string errorText) {
+            Success = success;
+            Response = response;
+            ResponseCode = responseCode;
+            Error = error;
+            ErrorText = errorText;
+        }
+
         public bool Success { get; internal set; }
 
         public S Response { get; internal set; }
